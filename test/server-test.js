09c500a3b1336c3d6aff837bfe54f46d0c494b58
Sarah Kirk <kirkse710@gmail.com>
2017-08-29 14:55:31 -0600

    gets the server.js to have three passing tests

--- test/server-test.js
+++ test/server-test.js
@@ -1,10 +1,43 @@
 const assert = require('chai').assert
 const app = require('../server')
+const request = require('request')
 
-describe('Server', function() {
+describe('Server', () => {
+    before(done => {
+      this.port = 9876
+      this.server = app.listen(this.port, (err, result) => {
+        if (err) { return done(err) }
+        done()
+      })
 
-  it('should exist', function() {
-    assert(app.get)
+      this.request = request.defaults({
+        baseUrl: 'http://localhost:9876/'
+      })
+    })
+
+    after(() => this.server.close())
+
+    it('should exist', () => {
+      assert(app)
+    })
+
+    describe('GET /', () => {
+      it('should return a 200', (done) => {
+        this.request.get('/', (error, response) => {
+          if (error) { done(error) }
+          assert.equal(response.statusCode, 200)
+          done()
+        })
+      })
+
+      it('should have a body with the name of the application', (done) => {
+        const title = app.locals.title
+
+        this.request.get('/', (error, response) => {
+          if (error) { done(error) }
+          assert(response.body.includes(title), `"${response.body}" does not include "${title}".`)
+          done()
+        })
+      })
+    })
   })
-
-})
