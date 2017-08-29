d96fbfe1d24a30826a766e3bfbc8f8fbae492333
Sarah Kirk <kirkse710@gmail.com>
2017-08-29 14:47:18 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+
+app.set('port', process.env.PORT || 3000)
+app.locals.title = 'Quantified Self Express API'
+
+app.get('/', (request, response) => {
+  response.send(app.locals.title)
+})
+
+if (!module.parent) {
+  app.listen(app.get('port'), () => {
+    console.log(`${app.locals.title} is running on ${app.get('port')}.`)
+  })
+}
+
+module.exports = app
