06817e641baa11964990a85d3a37ab64a9dc5271
Sarah Kirk <kirkse710@gmail.com>
2017-08-29 15:01:59 -0600

    commit

--- test/server-test.js
+++ test/server-test.js
@@ -3,41 +3,45 @@ const app = require('../server')
 const request = require('request')
 
 describe('Server', () => {
-    before(done => {
-      this.port = 9876
-      this.server = app.listen(this.port, (err, result) => {
-        if (err) { return done(err) }
+  before(done => {
+    this.port = 9876
+    this.server = app.listen(this.port, (err, result) => {
+      if (err) { return done(err) }
+      done()
+    })
+
+    this.request = request.defaults({
+      baseUrl: 'http://localhost:9876/'
+    })
+  })
+
+  after(() => this.server.close())
+
+  it('should exist', () => {
+    assert(app)
+  })
+
+  describe('GET /', () => {
+    it('should return a 200', (done) => {
+      this.request.get('/', (error, response) => {
+        if (error) { done(error) }
+        assert.equal(response.statusCode, 200)
         done()
       })
-
-      this.request = request.defaults({
-        baseUrl: 'http://localhost:9876/'
-      })
     })
 
-    after(() => this.server.close())
+    it('should have a body with the name of the application', (done) => {
+      const title = app.locals.title
 
-    it('should exist', () => {
-      assert(app)
-    })
-
-    describe('GET /', () => {
-      it('should return a 200', (done) => {
-        this.request.get('/', (error, response) => {
-          if (error) { done(error) }
-          assert.equal(response.statusCode, 200)
-          done()
-        })
-      })
-
-      it('should have a body with the name of the application', (done) => {
-        const title = app.locals.title
-
-        this.request.get('/', (error, response) => {
-          if (error) { done(error) }
-          assert(response.body.includes(title), `"${response.body}" does not include "${title}".`)
-          done()
-        })
+      this.request.get('/', (error, response) => {
+        if (error) { done(error) }
+        assert(response.body.includes(title), `"${response.body}" does not include "${title}".`)
+        done()
       })
     })
   })
+
+  describe('GET /api/v1/foods/:id', () => {
+    it('should return ')
+  })
+})
