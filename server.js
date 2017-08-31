9bc6dc0042a6f8ac01aef106fc15769d761b8b28
Adam Gunther <adamgunther1@gmail.com>
2017-08-31 13:49:08 -0600

    moves logic in show individual meal api into controller and out of route"

--- server.js
+++ server.js
@@ -38,15 +38,7 @@ app.get('/api/v1/meals', (request, response) => {
 })
 
 app.get('/api/v1/meals/:id/foods', (request, response) => {
-  const id = request.params.id
-  Meal.getMeal(id)
-  .then( (data) => {
-    Meal.getMealsFoods(id)
-    .then( (foods) => {
-      const meal = Meal.addFoodsToMeal(data, foods)
-      response.json(meal)
-    })
-  })
+  mealController.showMeal(request, response)
 })
 
 if (!module.parent) {
